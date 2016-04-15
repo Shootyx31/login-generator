1af2a06afe2d9c6abdc9802101bf7391d8ab11bd
Shootyx31 <shootyx31@hotmail.fr>
2016-04-15 14:19:09 +0200

    commit FindBug sur les fichier LoginService, LoginGenerator reduit de 40 a 13

--- src/main/java/dcll/LoginService.java
+++ src/main/java/dcll/LoginService.java
@@ -3,5 +3,74 @@ package dcll;
 /**
  * Created by valentin on 15/04/16.
  */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * Classe de service permettant la gestion des login.
+ */
 public class LoginService {
+
+    /**
+     * Declaration des logins existants.
+     */
+    private final ArrayList<String> loginsExistants;
+
+    /**
+     * Construit un login service a partir de la liste des logins existant.
+     *
+     * @param loginsExistants
+     */
+    public LoginService(final String[] loginsExistants) {
+        this.loginsExistants = new ArrayList<String>(Arrays.asList(loginsExistants));
+        Collections.sort(this.loginsExistants);
+    }
+
+    /**
+     * Verifie si un login existe deja.
+     *
+     * @param login le login a verifier
+     * @return true si le login existe deja, false sinon
+     */
+    public boolean loginExists(final String login) {
+        return loginsExistants.contains(login);
+    }
+
+    /**
+     * Ajoute un login a la liste des logins existants.
+     *
+     * @param login le login a ajouter
+     */
+    public void addLogin(final String login) {
+        loginsExistants.add(login);
+    }
+
+    /**
+     * Recherche tous les logins commençant par un meme prefixe.
+     *
+     * @param pref le prefixe
+     * @return la liste des logins prefixes par <em>pref</em>
+     */
+    public List<String> findAllLoginsStartingWith(final String pref) {
+        ArrayList<String> loginsPrefixes = new ArrayList<String>();
+        for (String login : loginsExistants) {
+            if (login.startsWith(pref)) {
+                loginsPrefixes.add(login);
+            }
+        }
+        return loginsPrefixes;
+    }
+
+    /**
+     * Recherche tous les logins.
+     *
+     * @return la liste de tous les logins
+     */
+    public List<String> findAllLogins() {
+        return loginsExistants;
+    }
 }
